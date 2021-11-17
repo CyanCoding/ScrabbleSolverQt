7b21fcf32dea4ddb939417f17bc4f02555dc1ae9
Camden <camden.macleod@pm.me>
2021-11-17 15:03:01 -0500

    Create project and grid

---
+++ mainwindow.cpp
@@ -0,0 +1,13 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+// Runs when the window is opened
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
+    ui->setupUi(this);
+}
+
+// Runs when the window is closed
+MainWindow::~MainWindow() {
+    delete ui;
+}
+
