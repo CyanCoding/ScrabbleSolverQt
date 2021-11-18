3245c0b2322d703c861ce957d149ab5c5488397d
Camden Mac Leod <camden.macleod@pm.me>
2021-11-18 11:04:17 -0500

    Add test functionality to fill two-dimensional array

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -11,8 +11,24 @@ MainWindow::~MainWindow() {
     delete ui;
 }
 
+// Fills in boardArray
+QString** fill_board_array(Ui::MainWindow* ui) {
+    QString** boardArray = new QString*[15];
+    for (int i = 0; i < 15; i++) {
+        boardArray[i] = new QString[15];
+    }
+
+    boardArray[0][0] = ui->box0x0->toPlainText();
+    ui->box0x10->setText(boardArray[0][0].toUpper());
+
+    return boardArray;
+    //MainWindow::boardArray[0][0] = ui->box0x0->toPlainText();
+    //ui->box0x3->setText(MainWindow::boardArray[0][0]);
+}
+
 // When the "Solve" button is pressed
 void MainWindow::on_pushButton_clicked() {
-
+    QString letters = ui->letterBox->toPlainText();
+    fill_board_array(ui);
 }
 
