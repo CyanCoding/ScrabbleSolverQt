bda4f05f0bdf3fba1a2ffd5a6eea4ccf77513b6c
Camden Mac Leod <camden.macleod@pm.me>
2021-11-20 10:58:45 -0500

    Create new board and copy old values

--- algorithm.cpp
+++ algorithm.cpp
@@ -6,5 +6,30 @@ namespace algorithm {
                                            QString** boardArray,
                                            std::vector<QString> dictionary) {
 
+        // Operate on every position
+        for (int i = 0; i < positions.size(); i++) {
+            // First we generate a new baord
+            QString** newBoard = new QString*[15];
+            for (int j = 0; j < 15; j++) {
+                newBoard[j] = new QString[15];
+            }
+
+            // Then copy from the old board to the new board
+            for (int j = 0; j < 15; j++) {
+                for (int k = 0; k < 15; k++) {
+                    // I have a hypothesis that if we simply copied every value
+                    // over, it would change boardArray whenever we loaded
+                    // new letters
+                    if (boardArray[j][k] != "") {
+                        newBoard[j][k] = boardArray[j][k];
+                    }
+                }
+            }
+
+
+        }
+
+
+
     }
 }
