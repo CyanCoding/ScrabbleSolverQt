d291601a3c968c2ec1c8127bf55f0a43fac85111
Camden Mac Leod <camden.macleod@pm.me>
2021-11-20 10:21:46 -0500

    Create generate_board function

--- algorithm.cpp
+++ algorithm.cpp
@@ -1,1 +1,7 @@
 #include "algorithm.h"
+
+namespace algorithm {
+    std::vector<QString**> generate_boards(std::vector<MainWindow::xy> positions, QString** boardArray) {
+
+    }
+}
--- algorithm.h
+++ algorithm.h
@@ -1,4 +1,12 @@
 #ifndef ALGORITHM_H
 #define ALGORITHM_H
 
+#include <vector>       // std::vector
+#include <QString>      // QString
+#include "mainwindow.h" // struct xy
+
+namespace algorithm {
+    std::vector<QString**> generate_boards(std::vector<MainWindow::xy> positions, QString** boardArray);
+}
+
 #endif // ALGORITHM_H
